3834c1608a3396028474b1337c3170be1674fe09
Dhruv <dhruv.mohan@outlook.com>
2021-01-05 17:08:20 +0530

    populate source.cpp

--- chain_playground/source.cpp
+++ chain_playground/source.cpp
@@ -2,10 +2,17 @@
 #include"add.h"
 #include"data.h"
 #include<memory>
+#include<iostream>
+
 void main() 
 {
 	std::shared_ptr<data> test_data = std::make_shared<data>(data(1, 2));
-	add algo_add(test_data);
-
+	auto algo_add = std::make_shared<algorithm>(add(test_data));
+	chain test_chain;
+	test_chain.add_algo(algo_add);
+	test_chain.execute();
+	std::cout << "A=" << test_data->a << std::endl;
+	std::cout << "B=" << test_data->b << std::endl;
+	
 
 }
